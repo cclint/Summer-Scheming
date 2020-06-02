a3a9cdd52ceb9080542f36d06d972d70cd388f95
Clint <clintac93@gmail.com>
2020-06-02 16:54:34 -0500

    add concepts

--- src/session2.rkt
+++ src/session2.rkt
@@ -43,4 +43,56 @@
  
  ;; END OF REVIEW 
 
- 
+
+;; Lists (the 4 main concepts)
+
+;; 1. empty list ()
+'() ;; evaluates to the empty list
+(list) ;; as an aside, this expression also evaluates to the empty list
+
+;; 2. cons (list constructor)
+(cons #f (cons 7 '()))
+;; 3. head (returns the first element in a list)
+(first '(1 2 3)) ;; returns 1
+;; =>
+1
+
+;; the first of the empty list produces an error
+
+;; 4. rest (returns the rest of the list)
+
+(rest '(1 2 3))
+;; =>
+;; (2 3)
+
+
+;; pairs
+
+;; 'cons' constructs pairs not lists
+;; lists are special types of pair structures (or the empty list)
+
+;; (cons 3 4)
+;;=>
+;; (3 . 4) this is a pair structure 
+
+(cons 1 (cons 2(cons 3 (cons 4 '()))) ;; returns ( 1 2 3 4)
+
+;; what is a list?
+;;
+;; inductive definition
+;; 1. the empty list [base case]
+;; 2. or, a cons pair whose cdr (or rest) is a list [inductive/recursive case]
+
+(list? '()) ;; returns #t (true), where list? is a predicate
+
+;; The real story:
+;; cons constructs pairs. some pairs are lists, but not all
+
+;;(cons(cons 1 2)(cons 3 4))
+;;=>
+;;((1 . 2) 3 . 4)
+;;
+;; (define x (cons(cons 1 2)(cons 3 4)))
+;; (cdar x) 
+;; =>
+;; 2
